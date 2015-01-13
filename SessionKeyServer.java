4a35bbdf1d5b0eee2c9d1084dc6150f6f1ac0205
Prateek Baranwal <prateek.baranwal@gmail.com>
2015-01-13 15:30:55 -0500

    Added -PF for taking tls password file as an input

--- SessionKeyServer.java
+++ SessionKeyServer.java
@@ -10,6 +10,7 @@ import java.net.InetAddress;
 import java.net.URI;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.Map;
 import java.util.HashMap;
@@ -51,6 +52,7 @@ public class SessionKeyServer {
 	    else if (args[i].equals("-l") && ++i < args.length) listen = args[i];
 	    else if (args[i].equals("-p") && ++i < args.length) port = Integer.parseInt(args[i]);
 	    else if (args[i].equals("-P") && ++i < args.length) tls_pwd = args[i];
+	    else if (args[i].equals("-PF") && ++i < args.length) tls_pwd = new String(new Scanner(new File(args[i])).useDelimiter("\\Z").next());
 	    else if (args[i].equals("-PP")) tls_pwd = new String(System.console().readPassword("TLS keystore+key password: "));
 	    else if (args[i].equals("-tls") && ++i < args.length) tls_ks = args[i];
 	    else if (args[i].equals("-h")) {
